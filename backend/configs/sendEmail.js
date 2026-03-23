393456ea9cc73faefd7659e1ba8303a5a1779487
Karanpatel10 <kbp4451@gmail.com>
2026-03-23 12:43:58 -0700

    fix email 4  setup

--- backend/configs/sendEmail.js
+++ backend/configs/sendEmail.js
@@ -1,4 +1,4 @@
-import * as Brevo from "@getbrevo/brevo";
+import {TransactionalEmailsApi} from "@getbrevo/brevo";
 
 const sendEmail = async (userEmail, booking) => {
   
@@ -8,10 +8,12 @@ const sendEmail = async (userEmail, booking) => {
 
       const apiInstance = new Brevo.TransactionalEmailsApi();
 
-    apiInstance.setApiKey(
-      Brevo.TransactionalEmailsApiApiKeys.apiKey,
-      process.env.SENDER_PASS
-    );
+      apiInstance.apiClient.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+
+    // apiInstance.setApiKey(
+    //   Brevo.TransactionalEmailsApiApiKeys.apiKey,
+    //   process.env.BREVO_API_KEY
+    // );
 
     const emailData = {
       sender: {
@@ -49,53 +51,5 @@ const sendEmail = async (userEmail, booking) => {
 }
 export default sendEmail;
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp-relay.brevo.com",
-//   port: 587,
-//   auth: {
-//     user: process.env.SENDER_USER,
-//     pass: process.env.SENDER_PASS,
-//   },
-// });
 
-// // Send an email using async/await
-// const sendEmail = async (userEmail, booking) => {
-//   console.log('Function sendEmail called with');
-//   console.log("Preparing to send email to:", userEmail);
-//   console.log("Booking details for email:", booking);
-//  try{ 
-//  const mailOptions = {
-//       from:process.env.SENDER_EMAIL,
-//       to: userEmail,
-//       subject: "Booking Confirmed 🎉",
-//       html: `
-
-//       <!-- LOGO -->
-//             <div style="text-align:center; margin-bottom:20px;">
-//             <img src="https://car-rental-cyan-delta.vercel.app/favicon.png" alt="Logo" width="120" />
-//             </div>
-
-//         <h2>Booking Successful 🎉</h2>
-//         <p>Your booking is confirmed.</p>
-
-//         <h3>Details:</h3>
-//         <ul>
-//           <li>Car: ${booking.car.brand} ${booking.car.model}</li>
-//           <li>Pickup: ${new Date(booking.pickupDate).toLocaleDateString()}</li>
-//           <li>Return: ${new Date(booking.returnDate).toLocaleDateString()}</li>
-//           <li>Total: $${booking.price}</li>
-//         </ul>
-
-//         <p>Thank you 🚗</p>
-//       `,
-//     };
-
-//    const info=await transporter.sendMail(mailOptions);
-//     console.log("Email sent");
-//  }catch(error){
-//   console.error("Email error:", error.message);
-//  }
-// }
-
-// export default sendEmail;
 
