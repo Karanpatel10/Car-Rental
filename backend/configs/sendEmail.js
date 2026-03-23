b7d18c42ea733da4d3752bf66ae2ac97b48fae21
Karanpatel10 <kbp4451@gmail.com>
2026-03-22 21:10:57 -0700

    receipt email

--- backend/configs/sendEmail.js
+++ backend/configs/sendEmail.js
@@ -1,22 +1,25 @@
-import Brevo from "@getbrevo/brevo";
+import nodemailer from "nodemailer";
 
+// Create a transporter using Ethereal test credentials.
+// For production, replace with your actual SMTP server details.
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.SENDER_USER,
+    pass: process.env.SENDER_PASS,
+  },
+});
+
+// Send an email using async/await
 const sendBookingEmail = async (userEmail, booking) => {
-  try {
-    const apiInstance = new Brevo.TransactionalEmailsApi();
-
-    apiInstance.setApiKey(
-      Brevo.TransactionalEmailsApiApiKeys.apiKey,
-      process.env.BREVO_API_KEY
-    );
-
-    const emailData = {
-      sender: {
-        email: process.env.SENDER_EMAIL,
-        name: process.env.SENDER_NAME,
-      },
-      to: [{ email: userEmail }],
+ try{
+ const mailOptions = {
+      from:`Car Rental <${transporter.options.auth.user}>`,
+      to: userEmail,
       subject: "Booking Confirmed 🎉",
-      htmlContent: `
+      html: `
 
       <!-- LOGO -->
             <div style="text-align:center; margin-bottom:20px;">
@@ -38,12 +41,12 @@ const sendBookingEmail = async (userEmail, booking) => {
       `,
     };
 
-    await apiInstance.sendTransacEmail(emailData);
-
+   const info=await transporter.sendMail(mailOptions);
     console.log("✅ Email sent");
-  } catch (error) {
-    console.error("❌ Email error:", error.message);
-  }
-};
+ }catch(error){
+  console.error("❌ Email error:", error.message);
+ }
+}
 
 export default sendBookingEmail;
+
